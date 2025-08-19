cdae92b7d88f9150a760ad93df766f582718efea
fento2 <ftonrate91@gmail.com>
2025-08-20 01:29:40 +0700

    Add Prisma setup with user model and update dependencies

---
+++ prisma/migrations/20250819172519_initial_accounts/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "public"."user" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_username_key" ON "public"."user"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "public"."user"("email");
